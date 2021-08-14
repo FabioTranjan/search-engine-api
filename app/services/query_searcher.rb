9c61614734fbd9d45f777bb1f78a0b3cf87ba477
Fabio <fabiotkx@gmail.com>
2021-08-14 19:03:44 -0300

    Added two more tests to search_at_google and search_at_bing.

--- app/services/query_searcher.rb
+++ app/services/query_searcher.rb
@@ -1,6 +1,6 @@
 class QuerySearcher
-  GOOGLE_BASE_SEARCH_URL= 'https://www.google.com.br/search'
-  BING_BASE_SEARCH_URL= 'https://www.bing.com/search'
+  GOOGLE_SEARCH_URL= 'https://www.google.com/search'
+  BING_SEARCH_URL= 'https://www.bing.com/search'
 
   def initialize
   end
@@ -9,13 +9,15 @@ class QuerySearcher
   end
 
   def search_at_google(query)
+    request(GOOGLE_SEARCH_URL, { q: query })
   end
 
   def search_at_bing(query)
+    request(BING_SEARCH_URL, { q: query })
   end
 
-  def request(url)
-    response = Faraday.get(url)
+  def request(url, params = {})
+    response = Faraday.get(url, params)
     raise StandardError.new(response.body) if response.status != 200
 
     response.body
